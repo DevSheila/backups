70b5f7562c1a3b4189f9266ea653833a9850d874
sheila <devsheilawambui@gmail.com>
2021-04-05 11:35:28 +0300

    ignore files

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { environment} from '../environments/environment'
+import { environment} from '../environments/environment.prod'
+// import { environment} from '../environments/environment'
+
 import { Observable, throwError } from 'rxjs';
 import {User} from './class/user'
 import {Repositories} from './class/repositories'
